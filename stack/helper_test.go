845f09485f6471eb17c4c5f0c87b93e9bdb9e785
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2015-10-15 15:09:24 +0900

    Updates as per the gofmt tool.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

--- stack/helper_test.go
+++ stack/helper_test.go
@@ -20,8 +20,8 @@ package stack
 import "bytes"
 
 func CompareStrings(this, other string) bool {
-        if this == "" || other == "" {
-                return false
-        }
-        return bytes.Equal([]byte(this), []byte(other))
+	if this == "" || other == "" {
+		return false
+	}
+	return bytes.Equal([]byte(this), []byte(other))
 }
